754ebf3a2ef2feaad0328b70b9d283ad76d44b8f
Nor121 <norbey.ramon@est.iudigital.edu.co>
2025-05-16 00:12:47 -0500

    Adaptación

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import Navbar from './components/Navbar.vue'
-import Home from './components/Home.vue'
 import Footer from './components/Footer.vue'
+import HomePage from './components/HomePage.vue';
 </script>
 
 <template>
-  <header>
-    <Navbar />
-  </header>
+    <header>
+      <Navbar />
+    </header>
 
-  <main>
-    <Home />
-  </main>
-  <Footer />
+    <main>
+    <HomePage />
+    </main>
+      <Footer />
 </template>
 
 <style scoped></style>
