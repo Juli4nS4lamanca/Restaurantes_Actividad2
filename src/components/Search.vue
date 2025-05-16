dd0f197d4b0588ce018b9231a4540636e898f27f
Nor121 <norbey.ramon@est.iudigital.edu.co>
2025-05-15 21:44:51 -0500

    Ajustarcodigo

---
+++ src/components/Search.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import HomePage from './views/HomePage.vue'; // Verifica que la ruta sea correcta
+import Footer from './components/AppFooter.vue';
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+
+  <main>
+    <HomePage />
+  </main>
+
+  <Footer />
+</template>
