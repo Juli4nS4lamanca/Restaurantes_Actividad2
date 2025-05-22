544f097d5a27062e889ce10488fdda956f011727
Nor121 <norbey.ramon@est.iudigital.edu.co>
2025-05-21 21:50:39 -0500

    Ultimoscambios

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Restaurantes_Actividad2/', // 👈 ¡Esta línea es clave
   plugins: [
     vue(),
     vueDevTools(),
