cfdaa9cc46a51d725852c64d398e6925bf50c128
Julian <julian.salamanca@est.iudigital.edu.co>
2025-05-27 18:25:24 -0500

    Arreglo

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Restaurantes_Actividad2/', // 👈 ¡Esta línea es clave
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
-
